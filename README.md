111ac6c551b558665518abef35485c3390d03848
saikrishnabudi <saikrishnabudi@outlook.com>
2024-04-11 20:44:21 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # End-To-End-NLP-Project
 End-To-End NLP Project Implementation
+
+## How to run?
+
+```bash
+conda create -n hate Python 3.12.3 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="saikrishnabudi",
+    author_email="saikrishnabudi@gmail.com",
+    packages=find_packages(),
+    install_requies=[],
+    
+)
